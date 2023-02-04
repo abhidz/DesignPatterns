97ff1c86edeb568433fb8a3b467f3cb5d11e110c
Abhishek kumar <abhishek.kumar@avalara.com>
2023-02-05 00:06:08 +0530

    Factory Method Implementation

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,149 @@
 ﻿namespace FactoryMethod
 {
-   
+    ///---------------------------------Product--------------------------------------------------
+   /// <summary>
+   /// Product
+   /// </summary>
+   public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    public class CountryDiscountServices : DiscountService
+    {
+        private readonly string _countryIdentifier  ;
+
+        public CountryDiscountServices(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "BE":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    public class CodeDiscountServices : DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountServices(Guid code)
+        {
+            _code = code;
+        }
+        public override int DiscountPercentage
+        {
+            get
+            {
+                return 15;
+            }
+        }
+    }
+
+    public class CityDiscountServices : DiscountService
+    {
+        private readonly string _cityName;
+
+        public CityDiscountServices(string cityName)
+        {
+            _cityName = cityName;
+        }
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_cityName)
+                {
+                    case "AS":
+                        return 10;
+                    default:
+                        return 12;
+                }
+            }
+        }
+    }
+
+    /// ----------------------------------------Creator----------------------------
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    /// <summary>
+    /// Concrete Creator
+    /// </summary>
+    /// 
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountServices(_countryIdentifier);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Creator
+    /// </summary>
+    /// 
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountServices(_code);
+        }
+    }
+
+    /// <summary>
+    /// Concrete Creator
+    /// </summary>
+    /// 
+    public class CityDiscountFactory : DiscountFactory
+    {
+        private readonly string _cityName;
+
+        public CityDiscountFactory(string cityName)
+        {
+            _cityName = cityName;
+        }
+        public override DiscountService CreateDiscountService()
+        {
+            return new CityDiscountServices(_cityName);
+        }
+    }
+
 }
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+var factories = new List<DiscountFactory>()
+{
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("BE"),
+    new CityDiscountFactory("RR")
+};
+
+foreach (var item in factories)
+{
+    var discountService = item.CreateDiscountService();
+    Console.WriteLine(discountService.DiscountPercentage + " " + discountService);
+}
+
+Console.ReadLine();
