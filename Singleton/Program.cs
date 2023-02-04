36053db9f8b15ca4727159a8206e023ae35a99db
Abhishek kumar <abhishek.kumar@avalara.com>
2023-02-04 22:11:40 +0530

    Implement Singelton pattern

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are same");
+}
+
+instance1.Log(nameof(instance1));
+instance2.Log(nameof(instance2));
+Logger.Instance.Log(nameof(Logger.Instance));
+Console.ReadLine();
