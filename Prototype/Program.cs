beb952e86bad5c1f59ee95c4c5564d2682448ade
Abhishek kumar <abhishek.kumar@avalara.com>
2023-02-06 18:14:32 +0530

    Prototype pattern

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.Title = "Prototype";
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Dan");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned : { managerClone.Name}");
+Console.WriteLine($"Manager was cloned : { manager.Name}");
+
+
+var employee = new Employee("Abhi", managerClone);
+var employeeClone = (Employee)employee.Clone();
+Console.WriteLine($"Employee was cloned : { employeeClone.Name} && {employeeClone.Manager.Name}");
+
+// chnage the manager name
+managerClone.Name = "David";
+Console.WriteLine($"Employee was cloned  : { employeeClone.Name} {employeeClone.Manager.Name}");
+Console.ReadLine();
